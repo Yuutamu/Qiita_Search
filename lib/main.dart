dfa82cb0e65bce40809185e24b7531a9c8a9c27f
Yuutamu <iwamoto.yuta1925@gmail.com>
2024-06-02 10:57:53 +0900

    flutter.dotenv

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart'; 
 
-void main() {
+// dotenv の読み込み処理（非同期で処理する）
+Future<void> main() async {
+  await dotenv.load(fileName: '.env'); // .env 読み込み
   runApp(const MainApp());
 }
 
